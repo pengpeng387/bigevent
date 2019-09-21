acb49619ab3a2be59926f5da112220635f29b571
pengpeng387 <l>
2019-09-21 21:29:15 +0800

    登陆功能完毕

---
+++ web_back/js/utils/config.js
@@ -0,0 +1,3 @@
+var URL = {
+    User_Login:"http://localhost:8000/admin/login"
+}
---
+++ web_back/js/utils/user.js
@@ -0,0 +1,19 @@
+var user = {
+    /**
+     * 用户名登陆
+     * @param {*} userName //用户名
+     * @param {*} password //密码
+     * @param {*} fn //回调函数
+     */
+    User_Login: function (userName,password,fn) {
+        $.ajax({
+            type: "post",
+            url: URL.User_Login,
+            data: { 'user_name': userName, 'password': password },
+            success: function (res) {
+                fn(res);
+            }
+        });
+    }
+}
+
